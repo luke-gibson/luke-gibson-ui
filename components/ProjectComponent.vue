15bec3ce8dee677283e2fbb42d641c8f73907493
Luke Gibson <hello@lukegibsondesign.co.uk>
2024-07-19 20:45:01 +0100

    updated projects page to be dynamic

--- components/ProjectComponent.vue
+++ components/ProjectComponent.vue
@@ -1,42 +1,71 @@
+<script setup lang="ts">
+  defineProps({
+    title: {
+      type: String,
+      required: true,
+    },
+    copy: {
+      type: String,
+      required: true,
+    },
+    link: {
+      type: String,
+      required: true,
+    },
+    image : {
+      type: String,
+      required: true,
+    },
+    alt: {
+      type: String,
+      required: true,
+    },
+    tags: {
+      type: Array,
+      required: true
+    }
+  });
+</script>
+
 <template>
   <li class="mb-12">
     <div class="group relative grid gap-4 pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
       <div class="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg"></div>
-      <div class="z-10 sm:order-2 sm:col-span-6">
+      <div class="z-10 sm:order-2 sm:col-span-5">
         <h3>
           <a
             class="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300 group/link text-base"
-            href="https://spectrumworkplace.co.uk/"
+            :href="link"
             target="_blank"
             rel="noreferrer noopener"
-            aria-label="...">
+            :aria-label="title">
             <span class="absolute -inset-x-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block"></span>
             <span>
-              Spectrum Workplace Website<IconsArrow></IconsArrow>
+              {{ title }} <IconsArrow></IconsArrow>
             </span>
           </a>
         </h3>
         <p class="mt-2 text-sm leading-normal">
-          Spectrum contacted me looking for a web developer to either fix or replace their
-          underperforming WordPress website. After careful consideration, we decided that
-          replacing the existing system with a more flexible and user-friendly platform
-          would be the best approach..
+          {{ copy }}
         </p>
+        <ul class="mt-2 flex flex-wrap" aria-label="Technologies used" v-if="tags.length">      
+        <BadgeComponent
+          v-for="tag in tags" 
+          :key="tag.name">
+          {{ tag.name }} 
+          </BadgeComponent>
+        </ul>
       </div>
       <nuxt-img
-        alt="Build a Spotify Connected App Newline course marketing card"
+        :alt="alt"
         loading="lazy"
         width="200"
-        height="48"
+        height="200"
         decoding="async"
         data-nimg="1"
-        class="rounded border-2 border-slate-200/10 transition group-hover:border-slate-200/30 sm:order-1 sm:col-span-2 sm:translate-y-1"
+        class="w-44 h-44 object-cover rounded-md transition group-hover:border-slate-200/30 sm:order-1 sm:col-span-3 sm:translate-y-1"
         style="color: transparent"
-        srcset="
-          https://res.cloudinary.com/dqqarxqbu/image/upload/v1645304762/Spectrum/spectrum_qrxpna.jpg 1x,
-          https://res.cloudinary.com/dqqarxqbu/image/upload/v1645304762/Spectrum/spectrum_qrxpna.jpg 2x
-        "
-        src="https://res.cloudinary.com/dqqarxqbu/image/upload/v1645304762/Spectrum/spectrum_qrxpna.jpg.jpg"
+        :src="image"
       ></nuxt-img>
     </div>
   </li>
--- pages/projects.vue
+++ pages/projects.vue
@@ -1,4 +1,52 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  const projects = [
+    {
+      image: "https://res.cloudinary.com/dqqarxqbu/image/upload/v1721416168/Luke%20Gibson%20Design/czone_suvy2t.jpg",
+      alt: "Czone - Image of a boat sailing",
+      title: "CZONE - Navico Group Employee",
+      copy: "As the Project Lead Developer at Navico Group, I built the CZone brand website using Optimizely CMS 12, focusing on SEO and technical performance. I created a user-friendly CMS experience that allows our web managers to quickly build high-quality websites without compromising on technical standards.",
+      link: "https://czone.navico.com",
+      tags: [
+        {
+          name: "Navico Group Employee",
+        },
+        {
+          name: "Lead Developer",
+        },
+        {
+          name: "Optimizely CMS 12",
+        },
+        {
+          name: "Assisted UX/ UI Design",
+        },
+      ]
+    },
+    {
+      image: "https://res.cloudinary.com/dqqarxqbu/image/upload/v1639579118/Luke%20Gibson%20Design/New%20site/spectrumWebP_eju1zo.webp",
+      alt: "Spectrum Workplace - Image of a finely designed and decorated office",
+      title: "SPECTRUM WORKPLACE",
+      copy: "I collaborated with SPECTRUM WORKPLACE to design and develop their website from scratch using the Umbraco CMS, empowering them with full control over their site’s content. This seamless integration ensures they can easily manage and update their digital presence to meet evolving business needs.",
+      link: "https://spectrumworkplace.co.uk/",
+      tags: [
+        {
+          name: "Freelance Developer",
+        },
+        {
+          name: "Front End Developer",
+        },
+        {
+          name: "Umbraco CMS",
+        },
+        {
+          name: "UX/ UI Design",
+        },
+        {
+          name: "Web Hosting/ Support",
+        },
+      ]
+    },
+  ]
+</script>
 
 <template>
   <section class="mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24" aria-label="Selected projects">
@@ -7,9 +55,15 @@
     </MobileTitle>
     <div>
       <ul class="group/list">
-        <ProjectComponent></ProjectComponent>
-        <ProjectComponent></ProjectComponent>
-        <ProjectComponent></ProjectComponent>
+        <ProjectComponent 
+          v-for="project in projects"
+          :image="project.image"
+          :alt="project.alt"
+          :title="project.title"
+          :copy="project.copy"
+          :link="project.link"
+          :tags="project.tags"
+          ></ProjectComponent>
       </ul>
     </div>
   </section>
