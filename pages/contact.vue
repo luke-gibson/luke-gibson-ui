506b1dc958f789ad9773735163f5467ad88f72dc
Luke Gibson <hello@lukegibsondesign.co.uk>
2024-08-10 23:13:56 +0100

    updated email address but still not working on arc

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,4 +1,7 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+    const emailAddress = "luke@lukegibson.co.uk";
+    const mailto = "mailto:" + emailAddress;
+</script>
 
 <template>
   <section class="flex flex-col align-middle" aria-label="Get in touch">        
@@ -19,7 +22,7 @@
             <NuxtLink rel="noopener noreferrer" target="_blank" to="https://wa.me/+447513822562" class="rounded-full bg-teal-400/10 hover:bg-teal-400/70 transition duration-300 px-5 py-3 text-xs font-medium leading-5 text-teal-300 hover:text-slate-950 mr-0 sm:mr-4 mb-4 max-h-[44px;] flex align-middle">
                 Send me a WhatsApp <IconsWhatsApp class="ml-2"></IconsWhatsApp>
             </NuxtLink>
-            <NuxtLink to="mailto:luke@lukegibson.co.uk" class="rounded-full bg-teal-400/10 hover:bg-teal-400/70 transition duration-300 px-5 py-3 text-xs font-medium leading-5 text-teal-300 hover:text-slate-950 mr-0 mb-10 max-h-[44px;] flex align-middle">
+            <NuxtLink rel="noopener noreferrer" target="_blank" :to="mailto" class="rounded-full bg-teal-400/10 hover:bg-teal-400/70 transition duration-300 px-5 py-3 text-xs font-medium leading-5 text-teal-300 hover:text-slate-950 mr-0 mb-10 max-h-[44px;] flex align-middle">
                 Send me an Email <IconsPlane class="ml-2"></IconsPlane>
             </NuxtLink>
         </div>
